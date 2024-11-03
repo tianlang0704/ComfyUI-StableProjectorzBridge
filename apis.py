0cf317c23e1718770bf2f462da66002cd78b35fd
CMonk <tianlang0704@gmail.com>
2024-11-03 16:34:12 +0800

    Fix bug when ComfyUI server port is not 8188 the images fails to upload

--- apis.py
+++ apis.py
@@ -141,7 +141,7 @@ async def upload_image_list(image_bytes_list, prefix, random_id, postfix):
         form.add_field('image', image_bytes, filename=filename, content_type='image/png')
         form.add_field('overwrite', 'true')
         async with aiohttp.ClientSession() as session:
-            await session.post('http://localhost:8188/upload/image', data = form)
+            await session.post(f'http://localhost:{PromptServer.instance.port}/upload/image', data = form)
 
 async def find_output_image_to_b64(output_prefix):
     images = []
